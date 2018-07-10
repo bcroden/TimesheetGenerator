c689f10ee3b79b940def109c9d3c535610142dfe
Brandon Roden <brandoncroden@gmail.com>
2018-07-09 19:16:53 -0500

    add print buttojn

--- src/App.js
+++ src/App.js
@@ -18,16 +18,14 @@ const formatDate = new Intl.DateTimeFormat(undefined, {
 
 class App extends React.Component {
     state = {
-        currentDate: new Date(),
-        isDefaultDate: true
+        currentDate: new Date()
     };
 
     datepicker = React.createRef();
 
     handleDateChange = (e, date) => {
         this.setState({
-            currentDate: new Date(date.getTime() + halfDayInMs),
-            isDefaultDate: false
+            currentDate: new Date(date.getTime() + halfDayInMs)
         });
     };
 
@@ -56,8 +54,12 @@ class App extends React.Component {
         }
     };
 
+    print = () => {
+        window.print();
+    };
+
     render() {
-        const { currentDate, isDefaultDate } = this.state;
+        const { currentDate } = this.state;
 
         return (
             <React.Fragment>
@@ -75,35 +77,50 @@ class App extends React.Component {
                 />
 
                 <div className="wrapper">
-                    {!isDefaultDate && (
-                        <div className="two-weeks-button">
-                            <div>2 weeks</div>
-                            <div className="row">
-                                <button onClick={this.subTwoWeeks}>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path d="M19 13H5v-2h14v2z" />
-                                        <path d="M0 0h24v24H0z" fill="none" />
-                                    </svg>
-                                </button>
-                                <button onClick={this.addTwoWeeks}>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
-                                        <path d="M0 0h24v24H0z" fill="none" />
-                                    </svg>
-                                </button>
-                            </div>
+                    <div className="two-weeks-button">
+                        <div className="row">
+                            <button onClick={this.subTwoWeeks}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M19 13H5v-2h14v2z" />
+                                    <path d="M0 0h24v24H0z" fill="none" />
+                                </svg>
+                            </button>
+                            <div className="two-weeks-label">2 weeks</div>
+                            <button onClick={this.addTwoWeeks}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+                                    <path d="M0 0h24v24H0z" fill="none" />
+                                </svg>
+                            </button>
                         </div>
-                    )}
+                        <div className="row">
+                            <button
+                                className="print-button"
+                                onClick={this.print}
+                            >
+                                Print
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z" />
+                                    <path d="M0 0h24v24H0z" fill="none" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
 
                     <Timesheet
                         currentDate={currentDate}
